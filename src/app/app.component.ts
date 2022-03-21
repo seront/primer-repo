108600c28a3439d57ea16adf1ab5e75b0be4d09f
Nelson Figuera <nelgab40@gmail.com>
2022-03-20 22:57:47 -0400

    error commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from './LoggingService.service';
 import { Persona } from './persona.model';
+import { PersonasService } from './personas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titulo = 'Listado de Personas';
-  personas: Persona[] = [
-    new Persona('Juan','Perez'), 
-    new Persona('Lara','Juanes'),
-    new Persona('Karla','Lara')
-];
-
-personaAgregada(persona: Persona){
-  this.personas.push( persona );
-}
+  personas: Persona[] = [];
+  constructor(private loggingService: LoggingService,
+              private personasService: PersonasService){}
+    
+   ngOnInit(): void {
+     this.personas = this.personasService.personas;
+      }
 
 }
+
+
---
+++ src/app/data.services.ts
@@ -0,0 +1,22 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Persona } from './persona.model';
+
+@Injectable()
+export class DataServices{
+    constructor(private httpClient: HttpClient){}
+    
+    cargarPersonas(){
+        return this.httpClient.get('https://listado-personas-3de42-default-rtdb.firebaseio.com/datos.json');
+    }
+    
+    //Guardar Personas
+    guardarPersonas(personas:Persona[]){
+     this.httpClient.put('https://listado-personas-3de42-default-rtdb.firebaseio.com/datos.json', personas) 
+     .subscribe(
+         response => console.log("resultado guardar Personas" + response),
+         error => console.log("Error al guardar Personas" + error)
+     );
+    }
+}
+ 
